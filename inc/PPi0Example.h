e28d1303e925e35c2ca2648aa874d6f480e95c54
Patrik Ott <ott@kph.uni-mainz.de>
2014-09-07 19:58:39 +0200

    reset PPio-example to the version before testing.

--- inc/PPi0Example.h
+++ inc/PPi0Example.h
@@ -4,26 +4,29 @@
 #include <iostream>
 #include <fstream>
 #include <cstdio>
-#include <string> 
+#include <string>
 
 #include "GTreeManager.h"
-#include "GH1.h"
+#include "PPhysics.h"
 
-class	PPi0Example : public GTreeManager
+class	PPi0Example  : public PPhysics
 {
 private:
     GH1*	time;
-     
+    GH1*	time_2g;
+
     GH1*	IM;
-    
+    GH1*	IM_2g;
+
     GH1*	MM;
-    
+    GH1*	MM_2g;
+
 protected:
     virtual Bool_t  Start();
 
     virtual void    ProcessEvent();
     virtual void	ProcessScalerRead();
-			
+
 public:
     PPi0Example();
     virtual ~PPi0Example();
--- src/PPi0Example.cc
+++ src/PPi0Example.cc
@@ -1,16 +1,20 @@
 #include "PPi0Example.h"
 
 PPi0Example::PPi0Example()
-{ 
+{
     GHistBGSub::InitCuts(-20, 15, -100, -40);
     GHistBGSub::AddRandCut(35, 95);
-    //GHistTaggerBinning::InitTaggerBinning(10, 20);
-        
+
+    SetTarget(938);
+
     time 	= new GH1("time", 	"time", 	1400, -700,	700);
+    time_2g = new GH1("time_2g","time_2g", 	1400, -700, 700);
 
     IM 		= new GH1("IM", 	"IM", 		400,   0,	400);
-  
+    IM_2g 	= new GH1("IM_2g", 	"IM_2g", 	400,   0, 	400);
+
     MM		= new GH1("MM", 	"MM", 	 	400,   800, 1200);
+    MM_2g	= new GH1("MM_2g", 	"MM_2g", 	400,   800, 1200);
 }
 
 PPi0Example::~PPi0Example()
@@ -28,28 +32,42 @@ Bool_t	PPi0Example::Start()
 
     TraverseValidEvents();
 
-	return kTRUE;
+    return kTRUE;
 }
 
 void	PPi0Example::ProcessEvent()
 {
-    for (Int_t i = 0; i < eta->GetNParticles(); i++)
+    // fill time diff (tagger - pi0), all pi0
+    FillTime(*pi0,time);
+
+    // fill missing mass, all pi0
+    FillMissingMass(*pi0,MM);
+
+    // fill invariant mass, all pi0
+    FillMass(*pi0,IM);
+
+    // Some neutral decays
+    for (Int_t i = 0; i < pi0->GetNParticles(); i++)
     {
-        for(int t=0; t<tagger->GetNTagged(); t++)
+        // Fill MM for 2 photon decay
+        if ((pi0->GetNSubParticles(i) == 2) & (pi0->GetNSubPhotons(i) == 2))
         {
-            time->Fill(tagger->GetTagged_t(t));
-            MM->Fill((tagger->GetVectorProtonTarget(t)-eta->Particle(i)).M(), tagger->GetTagged_t(t), tagger->GetTagged_ch(t));
+            // fill time diff (tagger - pi0), this pi0
+            FillTime(*pi0,i,time_2g);
+
+            // fill missing mass, this pi0
+            FillMissingMass(*pi0,i,MM_2g);
+
+            // fill invariant mass, this pi0
+            FillMass(*pi0,i,IM_2g);
         }
-        IM->Fill(eta->Particle(i).M(), *tagger, kTRUE);
-	}
-	
+
+    }
+
 }
 
 void	PPi0Example::ProcessScalerRead()
 {
-    time->ScalerReadCorrection(5);
 
-    IM->ScalerReadCorrection(5);
-
-    MM->ScalerReadCorrection(5);
+    //time.ScalerReadCorrection(5);
 }
