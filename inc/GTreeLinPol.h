8ff2b8c580dcf5417002a94074e85412b2ecd907
Cristina Collicott <collicott@dal.ca>
2014-10-14 17:44:29 +0200

    Robust LinPol Tree

--- inc/GTreeLinPol.h
+++ inc/GTreeLinPol.h
@@ -12,8 +12,11 @@
 class  GTreeLinPol : public GTree
 {
 private:
-    Int_t		plane;
+    Int_t	plane;
+    Double_t	edge;
+    Double_t	edgeSetting;    
     Double_t	polTable[352];
+    Double_t	enhTable[352];
 
 protected:
     virtual void    SetBranchAdresses();
@@ -24,9 +27,13 @@ public:
     virtual ~GTreeLinPol();
 
     virtual void        Clear()     {}
-            Int_t 		GetPolPlane() const	{return plane;}
-     const  Double_t*	GetPolTable() const	{return polTable;}
-
+            Int_t 	GetPolPlane() 	const	{return plane;}
+            Double_t 	GetEdge() 	const	{return edge;}
+            Double_t 	GetEdgeSetting()const	{return edgeSetting;}	    
+     const  Double_t*	GetPolTable() 	const	{return polTable;}
+     const  Double_t	GetPolDegree(Int_t channel) const	{return polTable[channel];}
+     const  Double_t*	GetEnhTable() 	const	{return enhTable;}
+     const  Double_t	GetEnhTable(Int_t channel) const	{return enhTable[channel];}
 
 };
 
--- src/GTreeLinPol.cc
+++ src/GTreeLinPol.cc
@@ -5,9 +5,16 @@
 
 GTreeLinPol::GTreeLinPol(GTreeManager *Manager)    :
     GTree(Manager, TString("treeLinPol"), kTRUE),
-    plane(0)
+    plane(0),
+    edge(0),
+    edgeSetting(0)
 {
-    for(int i=0; i<352; i++) polTable[i] = 0;
+    for(int i=0; i<352; i++)
+    {
+    	polTable[i] = 0;
+	enhTable[i] = 0;
+    }
+    
 }
 
 GTreeLinPol::~GTreeLinPol()
@@ -18,11 +25,18 @@ GTreeLinPol::~GTreeLinPol()
 void    GTreeLinPol::SetBranchAdresses()
 {
     tree_in->SetBranchAddress("plane", &plane);
+    tree_in->SetBranchAddress("edge", &edge);
+    tree_in->SetBranchAddress("edgeSetting", &edgeSetting);
     tree_in->SetBranchAddress("polTable", polTable);
+    tree_in->SetBranchAddress("enhTable", enhTable);
+    
 }
 
 void    GTreeLinPol::SetBranches()
 {
     tree_out->Branch("plane", &plane, "plane/I");
+    tree_out->Branch("edge", &edge, "edge/D");
+    tree_out->Branch("edgeSetting", &edgeSetting, "edgeSetting/D");
     tree_out->Branch("polTable", polTable, "polTable[352]/D");
+    tree_out->Branch("enhTable", enhTable, "enhTable[352]/D");
 }
