b2adade448777958678616130628e2db3f92f483
Oliver Steffen <steffen@kph.uni-mainz.de>
2015-03-31 16:45:33 +0200

    Ant: added another omega analysis: OmegaButtomUp

---
+++ ant/analysis/omega_bottomup.cc
@@ -0,0 +1,92 @@
+#include "omega_bottomup.h"
+#include "plot/root_draw.h"
+#include <algorithm>
+#include <iostream>
+
+using namespace ant;
+using namespace std;
+
+void ant::analysis::OmegaBottomUp::findPossibleDecays(const refRecParticle gamma, const refRecParticle meson_gamma_1, const refRecParticle meson_gamma_2, decaylist_t& decays)
+{
+
+    TLorentzVector meson_cand = TLorentzVector(*meson_gamma_1) + TLorentzVector(*meson_gamma_2);
+    const ParticleTypeDatabase::Type* t = nullptr;
+
+    if( eta_mass_cut.Contains(meson_cand.M())) {
+        t = &ParticleTypeDatabase::Eta;
+    } else if( pi0_mass_cut.Contains(meson_cand.M())) {
+        t = &ParticleTypeDatabase::Pi0;
+    }
+
+    if(t) {
+        TLorentzVector omega_cand = meson_cand + TLorentzVector(*gamma);
+
+        if(omega_mass_cut.Contains(omega_cand.M())) {
+            decays.emplace_back( omega_decay(
+                Particle(ParticleTypeDatabase::Omega, omega_cand),
+                Particle(*t, meson_cand))
+                                 );
+        }
+    }
+}
+
+ant::analysis::OmegaBottomUp::OmegaBottomUp():
+    eta_mass_cut(IntervalD::CenterWidth(ParticleTypeDatabase::Eta.Mass(), 50.0)),
+    pi0_mass_cut(IntervalD::CenterWidth(ParticleTypeDatabase::Pi0.Mass(), 20.0)),
+    omega_mass_cut(IntervalD::CenterWidth(ParticleTypeDatabase::Omega.Mass(), 80.0))
+{
+    HistogramFactory::SetName("Omega_ButtomUp");
+    omega_eta_found = HistWrap<int>::makeHist(
+                "#omega #rightarrow #eta #gamma #rightarrow (#gamma #gamma) #gamma per event",
+                "number of decays / event",
+                "",
+                BinSettings(10),
+                "omega_eta_per_event");
+    omega_pi0_found = HistWrap<int>::makeHist(
+                "#omega #rightarrow #pi^{0} #gamma #rightarrow (#gamma #gamma) #gamma per event",
+                "number of decays / event",
+                "",
+                BinSettings(10),
+                "omega_pi0_per_event");
+
+}
+
+void ant::analysis::OmegaBottomUp::ProcessEvent(const ant::Event &event)
+{
+
+    decaylist_t decays;
+
+    refRecParticleList_t photons = event.ParticleType(ParticleTypeDatabase::Photon);
+
+    sort(photons.begin(),photons.end(),[] (refRecParticle a, refRecParticle b) { return a->E() > b->E();});
+
+    if(photons.size() < 3) return;
+
+    findPossibleDecays(photons.at(0), photons.at(1), photons.at(2), decays);
+    findPossibleDecays(photons.at(1), photons.at(0), photons.at(2), decays);
+    findPossibleDecays(photons.at(2), photons.at(0), photons.at(1), decays);
+
+
+
+    int n_omega_eta =0;
+    int n_omega_pi0 =0;
+    for(auto& d : decays) {
+        if(d.meson2.Type() == ParticleTypeDatabase::Eta) {
+            n_omega_eta++;
+        } else if(d.meson2.Type() == ParticleTypeDatabase::Pi0) {
+            n_omega_pi0++;
+        }
+    }
+    omega_eta_found.Fill(n_omega_eta);
+    omega_pi0_found.Fill(n_omega_pi0);
+}
+
+void ant::analysis::OmegaBottomUp::Finish()
+{
+
+}
+
+void ant::analysis::OmegaBottomUp::ShowResult()
+{
+    canvas("Omega Buttom Up") << omega_eta_found << omega_pi0_found << endc;
+}
---
+++ ant/analysis/omega_bottomup.h
@@ -0,0 +1,45 @@
+#ifndef OMEGA_BOTTOMUP_H
+#define OMEGA_BOTTOMUP_H
+
+#include "AntPhysics.h"
+#include "plot/Hist.h"
+#include "base/interval.h"
+#include "Particle.h"
+
+
+#include <list>
+
+namespace ant {
+namespace analysis {
+
+class OmegaBottomUp: public ant::Physics {
+protected:
+
+    IntervalD eta_mass_cut;
+    IntervalD pi0_mass_cut;
+    IntervalD omega_mass_cut;
+
+    struct omega_decay {
+        omega_decay(Particle omega_, Particle meson2_): omega(omega_), meson2(meson2_) {}
+        Particle omega;
+        Particle meson2;
+    };
+
+    using decaylist_t = std::list<omega_decay>;
+
+    void findPossibleDecays(const refRecParticle gamma1, const refRecParticle meson_gamma_1, const refRecParticle meson_gamma_2, decaylist_t &decays);
+
+    HistWrap<int> omega_eta_found;
+    HistWrap<int> omega_pi0_found;
+
+public:
+    OmegaBottomUp();
+    virtual ~OmegaBottomUp() {}
+
+    virtual void ProcessEvent(const ant::Event& event);
+    virtual void Finish();
+    virtual void ShowResult();
+};
+}
+}
+#endif
