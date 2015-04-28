8640bf3d433d0a4b26a20d8e56cb82aac759e755
Phil Martel <philmartel0@gmail.com>
2015-04-28 13:31:22 +0200

    Added individual crystal energies and times into GTreeDetectorHits

--- inc/GTreeDetectorHits.h
+++ inc/GTreeDetectorHits.h
@@ -12,15 +12,23 @@ private:
     Int_t		nNaIHits;
     Int_t		NaIHits[720];
     Int_t		NaICluster[720];
+    Double_t    NaIEnergy[720];
+    Double_t    NaITime[720];
     Int_t		nPIDHits;
     Int_t		PIDHits[24];
+    Double_t    PIDEnergy[24];
+    Double_t    PIDTime[24];
     Int_t		nMWPCHits;
     Int_t		MWPCHits[860];
     Int_t		nBaF2Hits;
     Int_t		BaF2Hits[438];
     Int_t		BaF2Cluster[438];
+    Double_t    BaF2Energy[438];
+    Double_t    BaF2Time[438];
     Int_t		nVetoHits;
     Int_t		VetoHits[438];
+    Double_t    VetoEnergy[438];
+    Double_t    VetoTime[438];
 
 protected:
     virtual void    SetBranchAdresses();
@@ -37,10 +45,18 @@ public:
             Int_t		GetNaIHits(const Int_t index)	const	{return NaIHits[index];}
     const	Int_t*		GetNaICluster()           		const	{return NaICluster;}
             Int_t		GetNaICluster(const Int_t index)	const	{return NaICluster[index];}
+    const	Double_t*	GetNaIEnergy()           		const	{return NaIEnergy;}
+            Double_t	GetNaIEnergy(const Int_t index)	const	{return NaIEnergy[index];}
+    const	Double_t*	GetNaITime()           		const	{return NaITime;}
+            Double_t	GetNaITime(const Int_t index)	const	{return NaITime[index];}
 
             Int_t		GetNPIDHits()      			const	{return nPIDHits;}
     const	Int_t*		GetPIDHits()               	const	{return PIDHits;}
             Int_t		GetPIDHits(const Int_t index)	const	{return PIDHits[index];}
+    const	Double_t*	GetPIDEnergy()           		const	{return PIDEnergy;}
+            Double_t	GetPIDEnergy(const Int_t index)	const	{return PIDEnergy[index];}
+    const	Double_t*	GetPIDTime()           		const	{return PIDTime;}
+            Double_t	GetPIDTime(const Int_t index)	const	{return PIDTime[index];}
 
             Int_t		GetNMWPCHits()       			const	{return nMWPCHits;}
     const	Int_t*		GetMWPCHits()                	const	{return MWPCHits;}
@@ -51,10 +67,18 @@ public:
             Int_t		GetBaF2Hits(const Int_t index)	const	{return BaF2Hits[index];}
     const	Int_t*		GetBaF2Cluster()                    const	{return BaF2Cluster;}
             Int_t		GetBaF2Cluster(const Int_t index)	const	{return BaF2Cluster[index];}
+    const	Double_t*	GetBaF2Energy()           		const	{return BaF2Energy;}
+            Double_t	GetBaF2Energy(const Int_t index)	const	{return BaF2Energy[index];}
+    const	Double_t*	GetBaF2Time()           		const	{return BaF2Time;}
+            Double_t	GetBaF2Time(const Int_t index)	const	{return BaF2Time[index];}
 
             Int_t		GetNVetoHits()                 const	{return nVetoHits;}
     const	Int_t*		GetVetoHits()                  const	{return VetoHits;}
             Int_t		GetVetoHits(const Int_t index)	const	{return VetoHits[index];}
+    const	Double_t*	GetVetoEnergy()           		const	{return VetoEnergy;}
+            Double_t	GetVetoEnergy(const Int_t index)	const	{return VetoEnergy[index];}
+    const	Double_t*	GetVetoTime()           		const	{return VetoTime;}
+            Double_t	GetVetoTime(const Int_t index)	const	{return VetoTime[index];}
 };
 
 
--- src/GTreeDetectorHits.cc
+++ src/GTreeDetectorHits.cc
@@ -21,15 +21,23 @@ void    GTreeDetectorHits::SetBranchAdresses()
     inputTree->SetBranchAddress("nNaIHits", &nNaIHits);
     inputTree->SetBranchAddress("NaIHits", NaIHits);
     inputTree->SetBranchAddress("NaICluster", NaICluster);
+    inputTree->SetBranchAddress("NaIEnergy", NaIEnergy);
+    inputTree->SetBranchAddress("NaITime", NaITime);
     inputTree->SetBranchAddress("nPIDHits", &nPIDHits);
     inputTree->SetBranchAddress("PIDHits", PIDHits);
+    inputTree->SetBranchAddress("PIDEnergy", PIDEnergy);
+    inputTree->SetBranchAddress("PIDTime", PIDTime);
     inputTree->SetBranchAddress("nMWPCHits", &nMWPCHits);
     inputTree->SetBranchAddress("MWPCHits", MWPCHits);
     inputTree->SetBranchAddress("nBaF2Hits", &nBaF2Hits);
     inputTree->SetBranchAddress("BaF2Hits", BaF2Hits);
     inputTree->SetBranchAddress("BaF2Cluster", BaF2Cluster);
+    inputTree->SetBranchAddress("BaF2Energy", BaF2Energy);
+    inputTree->SetBranchAddress("BaF2Time", BaF2Time);
     inputTree->SetBranchAddress("nVetoHits", &nVetoHits);
     inputTree->SetBranchAddress("VetoHits", VetoHits);
+    inputTree->SetBranchAddress("VetoEnergy", VetoEnergy);
+    inputTree->SetBranchAddress("VetoTime", VetoTime);
 }
 
 void    GTreeDetectorHits::SetBranches()
@@ -37,15 +45,23 @@ void    GTreeDetectorHits::SetBranches()
     outputTree->Branch("nNaIHits", &nNaIHits, "nNaIHits/I");
     outputTree->Branch("NaIHits", NaIHits, "NaIHits[nNaIHits]/I");
     outputTree->Branch("NaICluster", NaICluster, "NaICluster[nNaIHits]/I");
+    outputTree->Branch("NaIEnergy", NaIEnergy, "NaIEnergy[nNaIHits]/D");
+    outputTree->Branch("NaITime", NaITime, "NaITime[nNaIHits]/D");
     outputTree->Branch("nPIDHits", &nPIDHits, "nPIDHits/I");
     outputTree->Branch("PIDHits", PIDHits, "PIDHits[nPIDHits]/I");
+    outputTree->Branch("PIDEnergy", PIDEnergy, "PIDEnergy[nPIDHits]/D");
+    outputTree->Branch("PIDTime", PIDTime, "PIDTime[nPIDHits]/D");
     outputTree->Branch("nMWPCHits", &nMWPCHits, "nMWPCHits/I");
     outputTree->Branch("MWPCHits", MWPCHits, "MWPCHits[nMWPCHits]/I");
     outputTree->Branch("nBaF2Hits", &nBaF2Hits, "nBaF2Hits/I");
     outputTree->Branch("BaF2Hits", BaF2Hits, "BaF2Hits[nBaF2Hits]/I");
     outputTree->Branch("BaF2Cluster", BaF2Cluster, "BaF2Cluster[nBaF2Hits]/I");
+    outputTree->Branch("BaF2Energy", BaF2Energy, "BaF2Energy[nBaF2Hits]/D");
+    outputTree->Branch("BaF2Time", BaF2Time, "BaF2Time[nBaF2Hits]/D");
     outputTree->Branch("nVetoHits", &nVetoHits, "nVetoHits/I");
     outputTree->Branch("VetoHits", VetoHits, "VetoHits[nVetoHits]/I");
+    outputTree->Branch("VetoEnergy", VetoEnergy, "VetoEnergy[nVetoHits]/D");
+    outputTree->Branch("VetoTime", VetoTime, "VetoTime[nVetoHits]/D");
 }
 
 
